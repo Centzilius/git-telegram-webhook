a1d48d7d32fd5f4009f8b3e7b81f696cb6776f47
Andreas Bielawski <brawl98@outlook.com>
2017-09-21 00:06:16 +0200

    Add repository name to output

--- webhook.py
+++ webhook.py
@@ -12,8 +12,15 @@ def process(chat_id):
     data = json.loads(request.data.decode())
     for commit in reversed(data["commits"]):
         bot.sendMessage(
-            chat_id=chat_id, 
-            text="<b>{0}</b> hat gerade <a href='{1}'>{2}</a> commited:\n<pre>{3}</pre>".format(commit["author"]["name"], commit["url"], commit["id"], html.escape(commit["message"])),
-            parse_mode="html",
-            disable_web_page_preview=True)
+            chat_id=chat_id,
+            text="<b>{0}</b> hat gerade <a href='{1}'>{2}</a> in <b>{3}</b> commited:\n<pre>{4}</pre>".format(
+                commit["author"]["name"],
+                commit["url"],
+                commit["id"],
+                data["repository"]["name"],
+                html.escape(commit["message"])
+            ),
+            parse_mode=telegram.ParseMode.HTML,
+            disable_web_page_preview=True
+        )
     return ""
